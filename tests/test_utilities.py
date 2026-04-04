d2202c002a5aba4e1fc45f3774d23f669ff75fb3
Isaac Waller <isaac.d.waller@gmail.com>
2026-04-04 22:46:27 +1000

    test: data-driven unit tests for utilities

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,11 +1,21 @@
+import pytest
 from pytest_mock import MockerFixture
 
 from granked_data_pipeline.utilities import extract_data
 
 
-def test_extract_data_success(mocker: MockerFixture):
+@pytest.mark.parametrize(
+    "status_code,expected_log_method,expected_outcome",
+    [
+        (200, "info", "succeeded"),
+        (500, "error", "failed"),
+    ],
+)
+def test_extract_data(
+    mocker: MockerFixture, status_code, expected_log_method, expected_outcome
+):
     mock_response = mocker.Mock()
-    mock_response.status_code = 200
+    mock_response.status_code = status_code
 
     mocker.patch(
         "granked_data_pipeline.utilities.requests.get", return_value=mock_response
@@ -17,25 +27,6 @@ def test_extract_data_success(mocker: MockerFixture):
     response = extract_data(mock_response.url, mock_logger, test_request_type)
     assert response == mock_response
 
-    mock_logger.info.assert_called_with(
-        f"{test_request_type.capitalize()} request succeeded url={mock_response.url} status_code={mock_response.status_code}"
-    )
-
-
-def test_extract_data_failure(mocker: MockerFixture):
-    mock_response = mocker.Mock()
-    mock_response.status_code = 500
-
-    mocker.patch(
-        "granked_data_pipeline.utilities.requests.get", return_value=mock_response
-    )
-
-    mock_logger = mocker.Mock()
-    test_request_type = "test request type"
-
-    response = extract_data(mock_response.url, mock_logger, test_request_type)
-    assert response == mock_response
-
-    mock_logger.error.assert_called_with(
-        f"{test_request_type.capitalize()} request failed url={mock_response.url} status_code={mock_response.status_code}"
+    getattr(mock_logger, expected_log_method).assert_called_with(
+        f"{test_request_type.capitalize()} request {expected_outcome} url={mock_response.url} status_code={mock_response.status_code}"
     )
